886f68e7505cab8a27b9d3334e624d7aca984ee6
dkfkqldk97 <dkfkqldk97@naver.com>
2023-11-06 22:20:42 +0900

    todo modal layout 수정

--- src/Component/today/Today.jsx
+++ src/Component/today/Today.jsx
@@ -10,32 +10,22 @@ const Today = () => {
     
     const getModal = (status) => {
         const modal = document.getElementById("todo_modal");
-        const section = document.getElementById("section");
-        console.log(section);
         //모달창 오픈 이벤트 && 모달창이 띄워져있는 경우
         if(status && modal != null) {
             modal.remove();
         }
 
-        if(status) {
-            section.style.setProperty('background', "#77767694");
-        } else {
-            section.style.setProperty('background', "#fff");
-        }
-
         setShowModal(status);
-        }
+    }
     
         return (
             <div className={styles.today_container}>
                 <div className={styles.today_title}>
                     ToDo List
                 </div>
-                <div className={styles.today_header}>
-                    <textarea className={styles.textarea}placeholder="search">
-                    </textarea>
-                </div>
-                <button className={styles.button_add} onClick={() => getModal(true)}>ADD</button>
+                <textarea className={styles.textarea}placeholder="search">
+                </textarea>
+                <button className={styles.button_add} onClick={() => getModal(true)}>+ ADD</button>
                 {showModal && createPortal(
                 <TodoModal onClose={() => getModal(false)} 
                 />, document.body
--- src/Component/todo/modal/TodoModal.jsx
+++ src/Component/todo/modal/TodoModal.jsx
@@ -1,14 +1,55 @@
 import React, { useState } from "react";
 import styles from "./TodoModal.module.css";
+import calanderImage from "./../../../Image/Vector.png";
 
 const TodoModal = (props) => {
-    const {onClose, month, day} = props;
+    const {onClose} = props;
+    const [starCalander, setStartCalander] = useState(
+            {
+                year : 2023
+                , month : 11
+                , day : 23
+            }
+    );
     
     return (
-        <div className={styles.modal_container} id="todo_modal">
-            {month}월{day}일
-            <button className={styles.modal_close}
-                onClick={onClose}>x</button>
+        <div className={styles.modal_bg}>
+            <div className={styles.modal_container} id="todo_modal">
+                <div className={styles.modal_header}>
+                    <button className={styles.modal_close} onClick={onClose}>x</button>
+                </div>
+                {/* 할일 입력 */}
+                <div className={styles.input_layout}>
+                    <textarea className={styles.input_text}placeholder="할일을 입력하세요"></textarea>
+                    <image src={calanderImage}></image>
+                </div>
+                <div>
+                {/* 기간 입력 */}
+                <div className={styles.calander_layout}>
+                    {/* 시작일자 */}
+                    <div>
+                        <span>{starCalander.year}</span>
+                        <span>{starCalander.month}</span>
+                        <span>{starCalander.day}</span>
+                    </div>
+                    {/* 기간 체크 */}
+                    <div className={styles.checkbox_layout}>
+                        <div className={styles.checkbox}></div>
+                        <p>기간 체크</p>
+                    </div>
+                </div>
+                {/* 태그 */}
+                <div>
+
+                </div>
+                {/* 메모 & 평가 */}
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+
+                </div>
+            </div>
         </div>
     )
 
